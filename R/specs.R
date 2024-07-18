721c8a62d868e57faa697fda358d2679f57e46d3
pvictor <perrier.victor@gmail.com>
2024-07-18 15:38:30 +0200

    set specs

--- R/specs.R
+++ R/specs.R
@@ -8,6 +8,9 @@
 #' @export
 #'
 #' @examples
+#' library(vchartr)
+#' data("mpg", package = "ggplot2")
+#'
 #' vbar(table(Class = mpg$class), aes(Class, Freq)) %>%
 #'   v_specs(
 #'     label = list(visible = TRUE),
@@ -36,13 +39,16 @@ v_specs <- function(vc, ...) {
 #' @export
 #'
 #' @examples
+#' library(vchartr)
+#' data("mpg", package = "ggplot2")
+#'
 #' vbar(table(Class = mpg$class), aes(Class, Freq)) %>%
 #'   v_labs(
 #'     title = "Title for the chart",
 #'     subtitle = "A subtitle to be placed under the title"
 #'   )
-v_labs <- function(vc, title, subtitle) {
-  .vchart_specs(
+v_labs <- function(vc, title, subtitle = NULL) {
+  vc <- .vchart_specs(
     vc,
     "title",
     list(
@@ -50,10 +56,92 @@ v_labs <- function(vc, title, subtitle) {
       subtext = subtitle
     )
   )
+  return(vc)
 }
 
 
 
+#' Set color(s) for chart
+#'
+#' @param vc An htmlwidget created with [vchart()] or specific chart's type function.
+#' @param ... Colors options, can be a single color code, a vector of colors to use or a list with more options.
+#'  For `v_colors_manual` it should be a named list with data values as name and color as values.
+#'
+#' @return A [vchart()] `htmlwidget` object.
+#' @export
+#'
+#' @name vc-colors
+#'
+#' @examples
+#' library(vchartr)
+#' data("mpg", package = "ggplot2")
+#'
+#' vbar(table(Class = mpg$class), aes(Class, Freq)) %>%
+#'   v_colors("#8FBCBB")
+#'
+#' vbar(
+#'   table(Class = mpg$class, Year = mpg$year),
+#'   aes(Class, Freq, fill = Year)
+#' ) %>%
+#'   v_colors_manual("2008" = "#88C0D0", "1999" = "#5E81AC")
+v_colors <- function(vc, ...) {
+  vc <- .vchart_specs(
+    vc,
+    "color",
+    list(...)
+  )
+  return(vc)
+}
+
+#' @export
+#'
+#' @rdname vc-colors
+v_colors_manual <- function(vc, ...) {
+  vc <- .vchart_specs(
+    vc,
+    "color",
+    list(specified = list(...))
+  )
+  return(vc)
+}
+
+
+
+
+#' Set legend options
+#'
+#' @param vc An htmlwidget created with [vchart()] or specific chart's type function.
+#' @param ... Options for the legend, see examples or online documentation :
+#'  [https://www.visactor.io/vchart/guide/tutorial_docs/Chart_Concepts/Legend](https://www.visactor.io/vchart/guide/tutorial_docs/Chart_Concepts/Legend).
+#'
+#' @return A [vchart()] `htmlwidget` object.
+#' @export
+#'
+#' @examples
+#' library(vchartr)
+#' data("mpg", package = "ggplot2")
+#'
+#' vbar(
+#'   table(Class = mpg$class, Year = mpg$year),
+#'   aes(Class, Freq, fill = Year)
+#' ) %>%
+#'   v_legend(
+#'     title = list(text = "Title", visible = TRUE),
+#'     orient = "right",
+#'     position = "start",
+#'     item = list(focus = TRUE)
+#'   )
+v_legend <- function(vc, ...) {
+  vc <- .vchart_specs(
+    vc,
+    "legends",
+    list(...)
+  )
+  return(vc)
+}
+
+
+#' @importFrom utils modifyList
 .vchart_specs <- function(vc, name, options) {
   stopifnot(
     "'vc' must be a 'vchart' htmlwidget object" = inherits(vc, "vchart")

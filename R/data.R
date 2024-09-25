753fa041715f3749563d57e143690e1a7e3a04ac
pvictor <victor.perrier@dreamRs.fr>
2024-09-25 15:07:49 +0200

    Error: Unicode character

--- R/data.R
+++ R/data.R
@@ -58,15 +58,15 @@
 #' @format A data frame with 495 observations and 11 variables:
 #'   * `country` : Country - Geographic location.
 #'   * `year` : Year - Year of observation.
-#'   * `co2` : Annual CO₂ emissions - Annual total emissions of carbon dioxide (CO₂), excluding land-use change, measured in million tonnes.
-#'   * `co2_per_gdp` : Annual CO₂ emissions per GDP (kg per international-$) - Annual total emissions of carbon dioxide (CO₂), excluding land-use change, measured in kilograms per dollar of GDP (2011 international-$).
-#'   * `co2_per_capita` : Annual CO₂ emissions (per capita) - Annual total emissions of carbon dioxide (CO₂), excluding land-use change, measured in tonnes per person.
-#'   * `co2_growth_abs` : Annual CO₂ emissions growth (abs) - Annual growth in total emissions of carbon dioxide (CO₂), excluding land-use change, measured in million tonnes.
-#'   * `co2_growth_prct` : Annual CO₂ emissions growth (%) - Annual percentage growth in total emissions of carbon dioxide (CO₂), excluding land-use change.
-#'   * `co2_per_unit_energy` : Annual CO₂ emissions per unit energy (kg per kilowatt-hour) - Annual total emissions of carbon dioxide (CO₂), excluding land-use change, measured in kilograms per kilowatt-hour of primary energy consumption.
-#'   * `consumption_co2` : Annual consumption-based CO₂ emissions - Annual consumption-based emissions of carbon dioxide (CO₂), measured in million tonnes.
-#'   * `consumption_co2_per_capita` : Per capita consumption-based CO₂ emissions - Annual consumption-based emissions of carbon dioxide (CO₂), measured in tonnes per person.
-#'   * `consumption_co2_per_gdp` : Annual consumption-based CO₂ emissions per GDP (kg per international-$) - Annual consumption-based emissions of carbon dioxide (CO₂), measured in kilograms per dollar of GDP (2011 international-$).
+#'   * `co2` : Annual CO2 emissions - Annual total emissions of carbon dioxide (CO2), excluding land-use change, measured in million tonnes.
+#'   * `co2_per_gdp` : Annual CO2 emissions per GDP (kg per international-$) - Annual total emissions of carbon dioxide (CO2), excluding land-use change, measured in kilograms per dollar of GDP (2011 international-$).
+#'   * `co2_per_capita` : Annual CO2 emissions (per capita) - Annual total emissions of carbon dioxide (CO2), excluding land-use change, measured in tonnes per person.
+#'   * `co2_growth_abs` : Annual CO2 emissions growth (abs) - Annual growth in total emissions of carbon dioxide (CO2), excluding land-use change, measured in million tonnes.
+#'   * `co2_growth_prct` : Annual CO2 emissions growth (%) - Annual percentage growth in total emissions of carbon dioxide (CO2), excluding land-use change.
+#'   * `co2_per_unit_energy` : Annual CO2 emissions per unit energy (kg per kilowatt-hour) - Annual total emissions of carbon dioxide (CO2), excluding land-use change, measured in kilograms per kilowatt-hour of primary energy consumption.
+#'   * `consumption_co2` : Annual consumption-based CO2 emissions - Annual consumption-based emissions of carbon dioxide (CO2), measured in million tonnes.
+#'   * `consumption_co2_per_capita` : Per capita consumption-based CO2 emissions - Annual consumption-based emissions of carbon dioxide (CO2), measured in tonnes per person.
+#'   * `consumption_co2_per_gdp` : Annual consumption-based CO2 emissions per GDP (kg per international-$) - Annual consumption-based emissions of carbon dioxide (CO2), measured in kilograms per dollar of GDP (2011 international-$).
 #'
 #' @note
 #' Documentation is from Our World In Data, see [https://github.com/owid/co2-data](https://github.com/owid/co2-data) for the data
@@ -84,8 +84,8 @@
 #' @format A data frame with 495 observations and 11 variables:
 #'   * `iso_code` : ISO code A3 for country.
 #'   * `name` : Name of country.
-#'   * `co2` : Annual CO₂ emissions - Annual total emissions of carbon dioxide (CO₂), excluding land-use change, measured in million tonnes.
-#'   * `co2_per_capita` : Annual CO₂ emissions (per capita) - Annual total emissions of carbon dioxide (CO₂), excluding land-use change, measured in tonnes per person.
+#'   * `co2` : Annual CO2 emissions - Annual total emissions of carbon dioxide (CO2), excluding land-use change, measured in million tonnes.
+#'   * `co2_per_capita` : Annual CO2 emissions (per capita) - Annual total emissions of carbon dioxide (CO2), excluding land-use change, measured in tonnes per person.
 #'   * `geometry` : Geographical attributes.
 #'
 #' @note
